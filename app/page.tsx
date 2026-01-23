d81191eb99701648a160f7874dbe11cefd6e364e
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-01-22 19:44:08 -0500

    feat(subheader): added subheader

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,9 @@ export default function Home() {
       <div className="flex relative justify-center top-45 w-220 mx-auto">
         <h1 className="font-[550] text-7xl text-center">Quantify your daily grind to <span className="bg-gradient-to-r from-violet-500 to-violet-600 bg-clip-text text-transparent">make ambition reality.</span></h1>
       </div>
+      <div className="flex relative justify-center top-50 w-240 mx-auto">
+        <h2 className="font-light text-white/80 text-[22px] text-center">Eliminate your feeling of falling behind with simple day-to-day tracking to reach your goals, from dominating your training to mastering your work and building a winning streak. </h2>
+      </div>
     </div>
   );
 }
