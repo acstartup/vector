919cb7024da027ab7f734b181c722984fca3b43e
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-01-31 15:27:34 -0500

    feat(base sign up): basic boxes and button for sign-up page, need backend

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Image from "next/image";
+import vector from "../../public/vector-full.png";
+import x from "../../public/vector-x.png"
+import check from "../../public/vector-check.png"
+import circle from "../../public/vector-circle.png"
+import eye from "../../public/vector-eye.png"
+
+export default function Home() {
+  return (
+    <div className="overflow-y-hidden min-h-screen">
+        <div className="flex flex-col items-center pl-1 pt-50"> {/* general positioning */}
+            <div className=""> {/* Vector. */}
+                <Image
+                    className="w-18 h-auto"
+                    src={vector}
+                    alt="Vector."
+                ></Image>
+            </div>
+            <h1 className="pt-2 text-4xl pl-1 font-light">Sign Up</h1>
+
+            <div className="flex flex-col pt-6 gap-4 pl-5"> {/* sign up boxes */}
+                <input
+                    className="outline-white outline-[1px] bg-white/10 w-70 h-7 px-3 rounded-xl text-sm"
+                    placeholder="Username"
+                ></input>
+                <input
+                    className="outline-white outline-[1px] bg-white/10 w-70 h-7 px-3 rounded-xl text-sm"
+                    placeholder="Password"
+                ></input>
+                <div className="relative flex flex-col bottom-1 pl-2 gap-1">
+                    <div className="flex flex-row items-baseline gap-2">
+                        <Image
+                            className="w-3 h-3"
+                            src={circle}
+                            alt="O"
+                        ></Image>
+                        <h1 className="relative text-xs bottom-0.5">At least 8 characters</h1>
+                    </div>
+                    <div className="flex flex-row items-baseline gap-2">
+                        <Image
+                            className="w-3 h-3"
+                            src={circle}
+                            alt="O"
+                        ></Image>
+                        <h1 className="relative text-xs bottom-0.5">Special symbol (@,!,$,etc...)</h1>
+                    </div>
+                    <div className="flex flex-row items-baseline gap-2">
+                        <Image
+                            className="w-3 h-3"
+                            src={circle}
+                            alt="O"
+                        ></Image>
+                        <h1 className="relative text-xs bottom-0.5">Uppercase letter (A-Z)</h1>
+                    </div>
+                    <div className="flex flex-row items-baseline gap-2">
+                        <Image
+                            className="w-3 h-3"
+                            src={circle}
+                            alt="O"
+                        ></Image>
+                        <h1 className="relative text-xs bottom-0.5">Lowercase letter (a-z)</h1>
+                    </div>
+                    <div className="flex flex-row items-baseline gap-2">
+                        <Image
+                            className="w-3 h-3"
+                            src={circle}
+                            alt="O"
+                        ></Image>
+                        <h1 className="relative text-xs bottom-0.5">Number (0-9)</h1>
+                    </div>
+                </div>
+                <div className="flex flex-row relative bottom-2.5">
+                    <input
+                        className="outline-white outline-[1px] bg-white/10 w-70 h-7 px-3 rounded-xl text-sm"
+                        placeholder="Confirm Password"
+                    ></input>
+                    <button className="relative right-8">
+                        <Image
+                            className="w-4 h-auto"
+                            src={eye}
+                            alt="()"
+                        ></Image>
+                    </button>
+                </div>
+                <div className="flex justify-center relative pr-2"> {/* sign up */}
+                    <button className="bg-white px-2 py-1 rounded-xl text-black text-sm hover:opacity-80">
+                        Sign Up
+                    </button>
+                </div>
+                <h2 className="flex justify-center relative bottom-1 pr-4 text-xs text-white/80">Already have an account?<a href="../log-in/" className="pl-1 text-white hover:underline">Log In</a></h2>
+            </div>
+        </div>
+    </div>
+  );
+}
